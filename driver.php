8f6b6227e244128609df4d95bc0970b89a7a89f8
micah <michaeleschbacher.com>
2013-04-12 09:05:15 -0500

    Initial Commit- procedural implementation

---
+++ driver.php
@@ -0,0 +1,81 @@
+<?php
+$deck = array();
+for ($i =0; $i< 52;$i++) {
+	$deck[] = $i % 13 + 1;
+}
+shuffle($deck);
+
+$player_one_cards = array();
+$player_two_cards = array();
+
+for ($i = 0; $i < 14; $i+=2) {
+	$player_one_cards[] = $deck[$i];
+	$player_two_cards[] = $deck[$i+1];
+}
+
+
+$game_is_playing = true;
+$player_one_score = 0;
+$player_two_score = 0;
+while (count($player_one_cards) > 0) {
+	echo "Which card would you like to play?\n";
+	foreach ($player_one_cards as $index=>$card) {
+		echo "{$index}) {$card}\n";
+	}
+	$input = readline("\n");
+
+	if (!array_key_exists($input, $player_one_cards)) {
+		echo "You did not enter a valid index\n";
+		die();
+	}
+
+	echo "You selected {$player_one_cards[$input]}\n";
+	$player_two = array_shift($player_two_cards);
+	echo "The computer played {$player_two}\n";
+	echo "\n";
+	if ($player_one_cards[$input] % 2 == 0) {
+		// player one is even
+		if ($player_two % 2 == 0) {
+			// Player two is even
+			echo "You both get 2 points for each being even\n";
+			$player_one_score+=2;
+			$player_two_score+=2;
+		} else {
+			// Player two is odd
+			echo "Computer played odd to your even. Computer gets 3 points\n";
+			$player_two_score+=3;
+		}
+	} else {
+		// player one is odd
+		if ($player_two % 2 == 0) {
+			// Player two is even
+			echo "You played odd to Computer's even. You get 3 points\n";
+			$player_one_score+=3;
+		} else {
+			// Player two is odd
+			echo "You both get 1 point for each being odd\n";
+			$player_one_score+=1;
+			$player_two_score+=1;
+	}
+}
+	unset($player_one_cards[$input]);
+	$player_one_cards = array_values($player_one_cards);
+
+	echo "\n";
+	$game_is_playing = false;
+}
+
+echo "\n".'Results:' . "\n";
+echo "You {$player_one_score}\n";
+echo "Computer:{$player_two_score}\n";
+echo "\n";
+if ($player_one_score == $player_two_score) {
+	echo "TIE\n";
+} elseif($player_one_score > $player_two_score) {
+	echo "You Win!\n";
+} else {
+	echo "You Lose!\n";
+
+}
+
+echo "\n";
